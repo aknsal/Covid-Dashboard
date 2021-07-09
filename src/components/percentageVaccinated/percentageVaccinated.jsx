bcbac8e56df2b2068b10b0207681be1028112331
Akshaj <akshajkansal@yahoo.com>
2021-07-09 22:31:01 +0530

    Add Pie Chart

--- src/components/percentageVaccinated/percentageVaccinated.jsx
+++ src/components/percentageVaccinated/percentageVaccinated.jsx
@@ -4,20 +4,75 @@ import {
   CardContent,
   Typography,
   Button,
+  Grid,
+  Container,
 } from "@material-ui/core";
+import PercentageVaccinatedChart from "../percentageVaccinatedChart/percentageVaccinatedChart";
 
 function PercentageVaccinated() {
   return (
-    <Card style={{ backgroundColor: "#c4ffc2" }}>
-      <CardHeader title="Percentage of Population Vaccintage"></CardHeader>
-      <CardContent>
-        <Typography variant="h6">1st Dose Vaccinated</Typography>
-        <Typography variant="h4">22%</Typography>
-        <br />
-        <Typography variant="h6">Fully Vaccinated</Typography>
-        <Typography variant="h4">4%</Typography>
-      </CardContent>
-    </Card>
+    <div>
+      <Card style={{ backgroundColor: "#c4ffc2" }}>
+        <CardHeader title="Percentage of Population Vaccinated"></CardHeader>
+
+        <PercentageVaccinatedChart />
+        <Container>
+          <Grid container spacing={1} justifyContent="center">
+            <Grid item>
+              <Grid container spacing={1} alignItems="center">
+                <Grid item>
+                  <div
+                    style={{
+                      backgroundColor: "#FFBB28",
+                      width: 15,
+                      height: 15,
+                    }}
+                  ></div>
+                </Grid>
+                <Grid item>
+                  <Typography variant="caption">Non-Vaccinated</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid item>
+              <Grid container spacing={1} alignItems="center">
+                <Grid item>
+                  <div
+                    style={{
+                      backgroundColor: "#0088FE",
+                      width: 15,
+                      height: 15,
+                    }}
+                  ></div>
+                </Grid>
+                <Grid item>
+                  <Typography variant="caption">Fully Vaccinated</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Grid container spacing={1} alignItems="center">
+                <Grid item>
+                  <div
+                    style={{
+                      backgroundColor: "#00C49F",
+                      width: 15,
+                      height: 15,
+                    }}
+                  ></div>
+                </Grid>
+                <Grid item>
+                  <Typography variant="caption">
+                    Partially Vaccinated
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Container>
+      </Card>
+    </div>
   );
 }
 
---
+++ src/components/percentageVaccinatedChart/percentageVaccinatedChart.jsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
+import Paper from "@material-ui/core/Paper";
+import "./percentageVaccinatedChart.css";
+
+function PercentageVaccinatedChart() {
+  const data = [
+    { name: "Fully Vaccinated", value: 3.46 },
+    { name: "Partially Vaccinated", value: 22.3 },
+    { name: "Non Vaccinated", value: 74.24 },
+  ];
+
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28"];
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="black"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  return (
+    <div className="chart">
+      <PieChart width={200} height={200}>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    </div>
+  );
+}
+
+export default PercentageVaccinatedChart;
